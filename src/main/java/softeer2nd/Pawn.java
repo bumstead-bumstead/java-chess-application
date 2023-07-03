a007be3eccb6b8e3732a3bf67a2aa45968329acb
yohwan <dldyghks951@gmail.com>
2023-07-03 01:45:42 -0700

    implement pawn

---
+++ src/main/java/softeer2nd/Pawn.java
@@ -0,0 +1,20 @@
+package softeer2nd;
+
+public class Pawn {
+    private String color;
+
+    public Pawn() {
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public Pawn(String color) {
+        this.color = color;
+    }
+}
---
+++ src/test/java/softeer2nd/PawnTest.java
@@ -0,0 +1,19 @@
+package softeer2nd;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PawnTest {
+
+    @Test
+    @DisplayName("흰색 폰이 생성되어야 한다")
+    public void create() {
+        Pawn pawn = new Pawn("white");
+        assertThat(pawn.getColor()).isEqualTo("white");
+
+        Pawn pawn2 = new Pawn("black");
+        assertThat(pawn2.getColor()).isEqualTo("black");
+    }
+}
