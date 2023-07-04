2c7c4750f6f625b72483a9b920b36cb7368e2e6e
yohwan <dldyghks951@gmail.com>
2023-07-04 21:58:47 +0900

    <feat> : Main의 체스 게임 run 기능 구현

--- src/main/java/softeer2nd/chess/Main.java
+++ src/main/java/softeer2nd/chess/Main.java
@@ -1,7 +1,35 @@
 package softeer2nd.chess;
 
+import java.util.Scanner;
+
 public class Main {
+    private static Scanner scanner = new Scanner(System.in);
+    private static Board board = new Board();
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        try {
+            run();
+        } catch (RuntimeException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private static void run() throws RuntimeException {
+        while (true) {
+            String command = scanner.nextLine();
+
+            if (command.equals("start")) {
+                System.out.println("게임을 시작합니다.");
+                board.initialize();
+                System.out.println(board.print());
+                continue;
+            }
+            if (command.equals("end")) {
+                System.out.println("게임을 종료합니다.");
+                return;
+            }
+
+            throw new IllegalArgumentException("잘못된 입력입니다.");
+        }
     }
 }
