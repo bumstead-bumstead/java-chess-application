3c739469089c175564310b78459d220b4813d329
yohwan <dldyghks951@gmail.com>
2023-07-05 11:54:54 +0900

    <feat> : Piece 팩토리 메서드 구현

--- src/main/java/softeer2nd/chess/pieces/Piece.java
+++ src/main/java/softeer2nd/chess/pieces/Piece.java
@@ -1,6 +1,7 @@
 package softeer2nd.chess.pieces;
 
 import softeer2nd.chess.PieceColor;
+import softeer2nd.chess.PieceRepresentation;
 import softeer2nd.chess.PieceType;
 
 public class Piece {
@@ -11,22 +12,77 @@ public class Piece {
 
     private final PieceColor pieceColor;
     private final PieceType pieceType;
+    private final PieceRepresentation representation;
 
-    private Piece(PieceColor pieceColor, PieceType pieceType) {
+
+    private Piece(PieceColor pieceColor, PieceType pieceType, PieceRepresentation representation) {
         this.pieceColor = pieceColor;
         this.pieceType = pieceType;
+        this.representation = representation;
     }
 
-    public static Piece createPiece(PieceColor pieceColor, PieceType pieceType) {
-        return new Piece(pieceColor, pieceType);
+    private static Piece createPiece(PieceColor pieceColor, PieceType pieceType, PieceRepresentation pieceRepresentation) {
+        return new Piece(pieceColor, pieceType, pieceRepresentation);
     }
 
-    public String getColor() {
-        return this.pieceColor.getColor();
+    public static Piece createWhitePawn() {
+        return new Piece(PieceColor.WHITE, PieceType.PAWN, PieceRepresentation.WHITE_PAWN);
     }
 
-    public char getRepresentation() {
-        return this.pieceColor.getRepresentation();
+    public static Piece createWhiteKnight() {
+        return new Piece(PieceColor.WHITE, PieceType.KNIGHT, PieceRepresentation.WHITE_KNIGHT);
+    }
+
+    public static Piece createWhiteKing() {
+        return new Piece(PieceColor.WHITE, PieceType.KING, PieceRepresentation.WHITE_KING);
+    }
+
+    public static Piece createWhiteQueen() {
+        return new Piece(PieceColor.WHITE, PieceType.QUEEN, PieceRepresentation.WHITE_QUEEN);
+    }
+
+    public static Piece createWhiteRook() {
+        return new Piece(PieceColor.WHITE, PieceType.ROOK, PieceRepresentation.WHITE_ROOK);
+    }
+
+    public static Piece createWhiteBishop() {
+        return new Piece(PieceColor.WHITE, PieceType.BISHOP, PieceRepresentation.WHITE_BISHOP);
+    }
+
+    public static Piece createBlackPawn() {
+        return new Piece(PieceColor.BLACK, PieceType.PAWN, PieceRepresentation.BLACK_PAWN);
+    }
+
+    public static Piece createBlackKnight() {
+        return new Piece(PieceColor.BLACK, PieceType.KNIGHT, PieceRepresentation.BLACK_KNIGHT);
+    }
+
+    public static Piece createBlackKing() {
+        return new Piece(PieceColor.BLACK, PieceType.KING, PieceRepresentation.BLACK_KING);
+    }
+
+    public static Piece createBlackQueen() {
+        return new Piece(PieceColor.BLACK, PieceType.QUEEN, PieceRepresentation.BLACK_QUEEN);
+    }
+
+    public static Piece createBlackRook() {
+        return new Piece(PieceColor.BLACK, PieceType.ROOK, PieceRepresentation.BLACK_ROOK);
+    }
+
+    public static Piece createBlackBishop() {
+        return new Piece(PieceColor.BLACK, PieceType.BISHOP, PieceRepresentation.BLACK_BISHOP);
+    }
+
+    public PieceColor getPieceColor() {
+        return pieceColor;
+    }
+
+    public PieceType getPieceType() {
+        return pieceType;
+    }
+
+    public PieceRepresentation getRepresentation() {
+        return representation;
     }
 
     @Override
