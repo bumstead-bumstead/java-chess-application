c05bf6f3ec53c295c1ee89ffc47985278469821a
yohwan <dldyghks951@gmail.com>
2023-07-05 11:54:32 +0900

    <test> : Piece create 매서드 테스트 코드 구현

--- src/test/java/softeer2nd/chess/pieces/PieceTest.java
+++ src/test/java/softeer2nd/chess/pieces/PieceTest.java
@@ -1,30 +1,36 @@
 package softeer2nd.chess.pieces;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import softeer2nd.chess.PieceColor;
+import softeer2nd.chess.PieceRepresentation;
+import softeer2nd.chess.PieceType;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PieceTest {
 
-
     @Test
-    public void create_기본생성자() throws Exception {
-        Piece piece = new Piece();
-        assertEquals(Piece.COLOR_WHITE, piece.getColor());
-        assertEquals(Piece.REPRESENTATION_WHITE, piece.getRepresentation());
+    @DisplayName("생성된 기물의 field가 유효해야한다.")
+    public void create_piece() {
+        verifyPiece(Piece.createBlackBishop(), PieceColor.BLACK, PieceType.BISHOP, PieceRepresentation.BLACK_BISHOP);
+        verifyPiece(Piece.createBlackRook(), PieceColor.BLACK, PieceType.ROOK, PieceRepresentation.BLACK_ROOK);
+        verifyPiece(Piece.createBlackKing(), PieceColor.BLACK, PieceType.KING, PieceRepresentation.BLACK_KING);
+        verifyPiece(Piece.createBlackKnight(), PieceColor.BLACK, PieceType.KNIGHT, PieceRepresentation.BLACK_KNIGHT);
+        verifyPiece(Piece.createBlackPawn(), PieceColor.BLACK, PieceType.PAWN, PieceRepresentation.BLACK_PAWN);
+        verifyPiece(Piece.createBlackQueen(), PieceColor.BLACK, PieceType.QUEEN, PieceRepresentation.BLACK_QUEEN);
+        verifyPiece(Piece.createWhiteBishop(), PieceColor.WHITE, PieceType.BISHOP, PieceRepresentation.WHITE_BISHOP);
+        verifyPiece(Piece.createWhiteRook(), PieceColor.WHITE, PieceType.ROOK, PieceRepresentation.WHITE_ROOK);
+        verifyPiece(Piece.createWhiteKing(), PieceColor.WHITE, PieceType.KING, PieceRepresentation.WHITE_KING);
+        verifyPiece(Piece.createWhiteKnight(), PieceColor.WHITE, PieceType.KNIGHT, PieceRepresentation.WHITE_KNIGHT);
+        verifyPiece(Piece.createWhitePawn(), PieceColor.WHITE, PieceType.PAWN, PieceRepresentation.WHITE_PAWN);
+        verifyPiece(Piece.createWhiteQueen(), PieceColor.WHITE, PieceType.QUEEN, PieceRepresentation.WHITE_QUEEN);
     }
 
-    @Test
-    public void create() {
-        verifyPawn(Piece.COLOR_WHITE);
-        verifyPawn(Piece.COLOR_BLACK);
-    }
-
-    void verifyPawn(final String color) {
-        Piece piece = new Piece(color);
-        assertEquals(color, piece.getColor());
-        assertEquals(PieceColor.valueOf(color.toUpperCase()).getRepresentation(), piece.getRepresentation());
+    private void verifyPiece(final Piece piece, final PieceColor pieceColor, final PieceType pieceType, final PieceRepresentation pieceRepresentation) {
+        assertEquals(pieceColor, piece.getPieceColor());
+        assertEquals(pieceRepresentation, piece.getRepresentation());
+        assertEquals(pieceType, piece.getPieceType());
     }
 }
