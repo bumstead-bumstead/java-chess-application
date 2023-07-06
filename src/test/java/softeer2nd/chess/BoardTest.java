48fd72d1d3a33269f9f6b387c16b200c6c63da5a
yohwan <dldyghks951@gmail.com>
2023-07-06 11:40:48 +0900

    <test> : Board.count() 메서드 테스트 코드 구현

--- src/test/java/softeer2nd/chess/BoardTest.java
+++ src/test/java/softeer2nd/chess/BoardTest.java
@@ -1,10 +1,11 @@
 package softeer2nd.chess;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import softeer2nd.chess.pieces.Piece;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import static softeer2nd.chess.utils.StringUtils.appendNewLine;
 
 class BoardTest {
@@ -13,11 +14,12 @@ class BoardTest {
     @BeforeEach
     public void setup() {
         board = new Board();
+        board.initialize();
     }
 
     @Test
+    @DisplayName("초기화된 보드의 상태를 확인한다.")
     public void create() throws Exception {
-        board.initialize();
         assertEquals(32, board.pieceCount());
         String blankRank = appendNewLine("........");
         assertEquals(
@@ -30,8 +32,16 @@ class BoardTest {
     }
 
     @Test
-    public void print() throws Exception {
-        board.initialize();
-        System.out.println(board.print());
+    @DisplayName("초기화된 상태의 보드의 검정색 폰의 개수는 8개를 반환해야한다.")
+    public void countBlackPawn() {
+        int numberOfBlackPawn = board.count(Piece.Color.BLACK, Piece.Type.PAWN);
+        assertEquals(numberOfBlackPawn, 8);
+    }
+
+    @Test
+    @DisplayName("초기화된 상태의 보드의 흰색 룩의 개수는 2개를 반환해야한다.")
+    public void countWhiteRook() {
+        int numberOfWhiteRook = board.count(Piece.Color.WHITE, Piece.Type.ROOK);
+        assertEquals(numberOfWhiteRook, 2);
     }
 }
