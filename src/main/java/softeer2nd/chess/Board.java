972bbef6ad59bc2127201f5edf4ecfd71fcf2063
yohwan <dldyghks951@gmail.com>
2023-07-06 11:33:55 +0900

    <refactor> : Board 2차원 배열의 빈 공간을 Piece 객체로 채우도록 리팩토링

--- src/main/java/softeer2nd/chess/Board.java
+++ src/main/java/softeer2nd/chess/Board.java
@@ -2,16 +2,14 @@ package softeer2nd.chess;
 
 import softeer2nd.chess.pieces.Piece;
 
-import static softeer2nd.chess.utils.StringUtils.appendNewLine;
-
 import java.util.ArrayList;
 import java.util.List;
+
+import static softeer2nd.chess.utils.StringUtils.appendNewLine;
 
 public class Board {
 
     private final static int BOARD_LENGTH = 8;
-    private final static int BLACK_PAWN_LINE_NUMBER = 1;
-    private final static int WHITE_PAWN_LINE_NUMBER = 6;
     private List<List<Piece>> pieces;
 
 
@@ -24,12 +22,54 @@ public class Board {
 
         fillFirstBlackRow();
         fillOneRow(Piece.createBlackPawn());
-        fillOneRow(null);
-        fillOneRow(null);
-        fillOneRow(null);
-        fillOneRow(null);
+        fillOneRow(Piece.createBlank());
+        fillOneRow(Piece.createBlank());
+        fillOneRow(Piece.createBlank());
+        fillOneRow(Piece.createBlank());
         fillOneRow(Piece.createWhitePawn());
         fillFirstWhiteRow();
+    }
+
+    public String showBoard() {
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < BOARD_LENGTH; i++) {
+            List<Piece> line = pieces.get(i);
+
+            result.append(appendNewLine(joinListToString(line)));
+        }
+        return result.toString();
+    }
+
+    public int pieceCount() {
+        int numberOfPieces = 0;
+
+        for (int row = 0; row < BOARD_LENGTH; row++) {
+            numberOfPieces += getNumberOfPiecesOfOneRow(row);
+        }
+
+        return numberOfPieces;
+    }
+
+    private int getNumberOfPiecesOfOneRow(int row) {
+        int numberOfPieces = 0;
+        for (int column = 0; column < BOARD_LENGTH; column++) {
+            if (pieces.get(row).get(column).getType().equals(Piece.Type.NO_PIECE)) {
+                continue;
+            }
+            numberOfPieces++;
+        }
+        return numberOfPieces;
+    }
+
+    private String joinListToString(List<Piece> line) {
+        StringBuilder result = new StringBuilder();
+
+        for (Piece piece : line) {
+            result.append(piece.getRepresentation());
+        }
+
+        return result.toString();
     }
 
     private void fillFirstBlackRow() {
@@ -68,65 +108,5 @@ public class Board {
         }
 
         pieces.add(row);
-    }
-
-    public String print() {
-        StringBuilder result = new StringBuilder();
-
-        for (int i = 0; i < BOARD_LENGTH; i++) {
-            List<Piece> line = pieces.get(i);
-
-            result.append(appendNewLine(joinListToString(line)));
-        }
-
-        return result.toString();
-    }
-
-    private String joinListToString(List<Piece> line) {
-        StringBuilder result = new StringBuilder();
-
-        for (Piece piece : line) {
-            if (piece == null) {
-                result.append(".");
-                continue;
-            }
-            result.append(piece);
-        }
-
-        return result.toString();
-    }
-
-    public String showBoard() {
-        StringBuilder result = new StringBuilder();
-
-        for (int i = 0; i < BOARD_LENGTH; i++) {
-            List<Piece> line = pieces.get(i);
-
-            result.append(appendNewLine(joinListToString(line)));
-        }
-
-        return result.toString();
-
-    }
-
-    public int pieceCount() {
-        int numberOfPieces = 0;
-
-        for (int row = 0; row < BOARD_LENGTH; row++) {
-            numberOfPieces += getNumberOfPiecesOfOneRow(row);
-        }
-
-        return numberOfPieces;
-    }
-
-    private int getNumberOfPiecesOfOneRow(int row) {
-        int numberOfPieces = 0;
-        for (int column = 0; column < BOARD_LENGTH; column++) {
-            if (pieces.get(row).get(column) == null) {
-                continue;
-            }
-            numberOfPieces++;
-        }
-        return numberOfPieces;
     }
 }
