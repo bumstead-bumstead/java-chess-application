c6e50ffdee771c38b097d2683f247b4eff775350
yohwan <dldyghks951@gmail.com>
2023-07-11 23:37:19 +0900

    <test> ChessGame pawn 이동 검증 기능 테스트 코드 구현

--- src/test/java/softeer2nd/chess/ChessGameTest.java
+++ src/test/java/softeer2nd/chess/ChessGameTest.java
@@ -3,12 +3,17 @@ package softeer2nd.chess;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import softeer2nd.chess.domain.Position;
-import softeer2nd.chess.exceptions.IllegalCommandException;
 import softeer2nd.chess.domain.pieces.Blank;
 import softeer2nd.chess.domain.pieces.Pawn;
 import softeer2nd.chess.domain.pieces.Piece;
+import softeer2nd.chess.exceptions.IllegalCommandException;
 import softeer2nd.chess.utils.ChessPositionParser;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -24,6 +29,7 @@ public class ChessGameTest {
         chessGame.start();
         turn = Piece.Color.BLACK;
     }
+
     @Test
     @DisplayName("게임이 시작한 직후에는 점수로 각각 38점을 가져야한다.")
     public void caculcatePoint() throws Exception {
@@ -32,16 +38,6 @@ public class ChessGameTest {
         assertEquals(38.0, chessGame.calculatePoint(Piece.Color.WHITE), 0.01);
         assertEquals(38.0, chessGame.calculatePoint(Piece.Color.BLACK), 0.01);
     }
-//    @Test
-//    @DisplayName("같은 열에 폰이 두 개 이상 있는 경우 개당 0.5점을 갖는다.")
-//    public void calculatePointWithMultiplePawns() throws Exception {
-//        chessGame.start();
-//
-//        chessGame.move("a2", "b7", turn);
-//
-//        assertEquals(37, chessGame.calculatePoint(Piece.Color.WHITE), 0.01);
-//        assertEquals(37, chessGame.calculatePoint(Piece.Color.BLACK), 0.01);
-//    }
 
     @Test
     @DisplayName("기물 이동 테스트")
@@ -72,5 +68,45 @@ public class ChessGameTest {
         Position targetPosition = ChessPositionParser.parse("a6");
 
         assertThrows(IllegalCommandException.class, () -> chessGame.move(sourcePosition, targetPosition, turn));
+    }
+
+    @ParameterizedTest
+    @MethodSource("providePositionsForVerifyPwnDiagonalMove")
+    @DisplayName("폰은 상대 기물이 없을 때 대각선 이동이 불가능하다.")
+    void verifyPawnDiagonalMoveFail(Position targetPosition) {
+        Position sourcePosition = new Position(1, 3);
+
+        assertThrows(IllegalCommandException.class, () -> chessGame.move(sourcePosition, targetPosition, turn));
+    }
+
+    private static Stream<Arguments> providePositionsForVerifyPwnDiagonalMove() {
+        return Stream.of(
+                Arguments.of(new Position(2, 4)),
+                Arguments.of(new Position(2, 2))
+        );
+    }
+
+    @Test
+    @DisplayName("폰은 상대 기물이 있을 때 대각선 이동이 가능하다.")
+    void verifyPawnDiagonalMove() {
+        Position sourcePosition = new Position(1, 1);
+        Position targetPosition = new Position(2, 0);
+
+        chessGame.move(new Position(6, 0), new Position(5, 0), Piece.Color.WHITE);
+        chessGame.move(new Position(5, 0), new Position(4, 0), Piece.Color.WHITE);
+        chessGame.move(new Position(4, 0), new Position(3, 0), Piece.Color.WHITE);
+        chessGame.move(new Position(3, 0), targetPosition, Piece.Color.WHITE);
+
+        chessGame.move(sourcePosition, targetPosition, turn);
+    }
+
+    @Test
+    @DisplayName("폰은 이동한 적이 없을 때만 double push할 수 있다.")
+    void verifyPawnDoublePush() {
+        Position sourcePosition = new Position(1, 3);
+
+        chessGame.move(sourcePosition, new Position(3, 3), turn);
+        assertThrows(IllegalCommandException.class, () -> chessGame.move(sourcePosition, new Position(5, 3), turn));
+
     }
 }
